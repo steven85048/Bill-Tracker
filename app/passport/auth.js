a7058aa74087cca6a7be665a9babc1a0dd960dd7
Steven Tran <steven.tran.4@asu.edu>
2017-04-30 23:11:25 -0700

    Update auth.js

--- app/passport/auth.js
+++ app/passport/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'googleAuth' : {
-		'clientId' : '266575854000-20vl4vk55a5mhv765v34gc0o7k2jfm4a.apps.googleusercontent.com',
-		'clientSecret' : 'YwHyY5K7crt2EzZUFRlQzDlZ',
+		'clientId' : 'Client_ID',
+		'clientSecret' : 'Client_Secret',
 		'callbackURL' : 'https://damp-cove-80159.herokuapp.com/auth/google/callback',
 	}
 } 
